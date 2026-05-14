6690d7533d204d0a796b62589cc2e1e5ba418d32
dtom-ass <tomas.jarasolano@gmail.com>
2026-05-14 15:59:52 -0500

    feat: Se implementa una consola gráfica con Swing para el sistema de logs.

--- src/view/console/ConsoleLog.java
+++ src/view/console/ConsoleLog.java
@@ -1,8 +1,38 @@
 package view.console;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class ConsoleLog {
-    public String message;
+
+    private static JTextArea textArea;
+
+    // Bloque static: se ejecuta una sola vez
+    static {
+
+        JFrame frame = new JFrame("Consola del Juego");
+
+        textArea = new JTextArea();
+        textArea.setEditable(false);
+
+        JScrollPane scrollPane = new JScrollPane(textArea);
+
+        frame.setLayout(new BorderLayout());
+        frame.add(scrollPane, BorderLayout.CENTER);
+
+        frame.setSize(700, 400);
+        frame.setLocationRelativeTo(null);
+
+        frame.setVisible(true);
+    }
+
     public static void Log(String message){
-        System.out.println("| LOG |: " + message);
+
+        textArea.append("|| LOG ||: " + message + "\n");
+
+        // Auto-scroll
+        textArea.setCaretPosition(
+                textArea.getDocument().getLength()
+        );
     }
 }
